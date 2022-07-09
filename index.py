171eacb5adc609564f5f7c70cccf7aab46175852
anonymouscoder999 <jlche18659@gmail.com>
2022-07-09 11:05:18 +0800

    converted tabs to spaces

--- index.py
+++ index.py
@@ -8,13 +8,13 @@ app.static_folder = "static"
  
 @app.route('/', methods =["GET", "POST"])
 def process():
-    if request.method == "POST":
-        numbers = []
-        for i in range(1, 5):
-            numbers.append(int(request.form.get("number{}".format(str(i)))))
-        answer = solver.solve(numbers)
-        return render_template("index.html", answer=answer)
-    return render_template("index.html", answer="")
+	if request.method == "POST":
+		numbers = []
+		for i in range(1, 5):
+			numbers.append(int(request.form.get("number{}".format(str(i)))))
+		answer = solver.solve(numbers)
+		return render_template("index.html", answer=answer)
+	return render_template("index.html", answer="")
  
 if __name__=='__main__':
    app.run()
